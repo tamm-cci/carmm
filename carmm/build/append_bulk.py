361dae40145837f3eb6e7421039f0d0a4d355e67
OscarvanVuren <vanvureno@cardiff.ac.uk>
2025-09-26 14:17:59 +0100

    Initial commit

---
+++ carmm/build/append_bulk.py
@@ -0,0 +1,41 @@
+
+def appending(surface, bulk, sep, output):
+    
+    """
+    Args:
+    	surface: file containing relaxed and optimised surface geometry without constraints (.traj).
+    	bulk: file containing relaxed and optimised bulk geometry without constraints (.traj). Must contain the number of layers of bulk required.
+    	sep: distance (Angstrom) between two layers. If sep = 0, then the surface will sit directly on bulk with the distance between them the same as the distance between the first upper 2 layers of bulk
+    	output: name of file that will be saved (.traj) of appended bulk and surface.
+    Returns:
+    """
+    
+    from ase.io import read, write
+    import numpy as np
+    
+    # Defining surface
+    surface = read(surface)
+    # Defining bulk
+    bulk = read(bulk)
+    
+    # Appending them
+    # Getting maximum coords of bulk in z direction
+    bulk_max_z = np.max(bulk.positions[:, 2])
+    # Getting minimum coords of surf in z direction
+    surf_min_z = np.min(surface.positions[:, 2])
+    
+    # Defining vertical distance between bulk and surf
+    separation = sep
+    z_shift = bulk_max_z - surf_min_z + separation
+    surface.positions[:, 2] += z_shift # Apply transition to surf
+    appended_material = bulk + surface # Combining bulk and surf into new object
+    
+    # Resizing cell and centering it
+    final_cell = appended_material.get_cell()
+    final_cell[2, 2] = np.max(appended_material.positions[:, 2]) - np.min(appended_material.positions[:, 2]) + 10.0
+    appended_material.set_cell(final_cell)
+    appended_material.center(axis=2)
+    
+    # Saving it
+    write(output, appended_material)
+    write(output, appended_material, append=True)
