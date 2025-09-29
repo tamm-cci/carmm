4e15117a442b410a86a4478b68b62ad0d29b951a
akash <akashhiregange1999@gmail.com>
2025-09-29 12:22:35 +0100

    made changes to the functions to be consistent. docstrings added to all the functions

--- carmm/phonon/post_process.py
+++ carmm/phonon/post_process.py
@@ -1,4 +1,26 @@
 def get_band_conf(atoms):
+    """
+    Generate the band structure configuration (band.conf file) for a given atomic structure.
+    The band.conf file is necessary to generate a phonon bandstructure once the force set are collected.
+
+    Parameters
+    ----------
+    atoms : ase.Atoms
+        structure of the unit cell as ase.Atoms object to generate the high symmetry points in the reciprocal space.
+
+    Returns
+    -------
+    None
+    Generates a band.conf file in the working directory that will be required for further post-processing.
+
+    Notes
+    -----
+    - This function serves as a helper to prepare configuration file to generate the phonon bandstructure.
+    - The exact k-path convention may depend on the crystal system
+
+    """
+
+
     lat1 = atoms.cell.bandpath()
     band_label = []
     band = []
@@ -42,20 +64,118 @@ def get_band_conf(atoms):
 
 
 def get_thermal_conf():
+    """
+    Generate the thermal properties configuration (thernal.conf file)
+    The thermal.conf file is necessary to generate a thermal properties data once the force set are collected and the
+    phonon bandstructure is generated.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    Generates a thermal.conf file in the working directory that will be required for further post-processing.
+
+    Notes
+    -----
+    - This function serves as a helper to prepare configuration file to generate the thermal property data like
+    energy, entropy, Helmholtz free energy and heat capacity.
+    - NOTE: currently the temperature range is from 0 to 1000 K. For a majority of systems, the range is sufficient to
+    understand the thermal properities. Future development may allow for custom range.
+
+    """
     f = open('thermal.conf', 'w')
     f.write(f'TPROP =.TRUE.\n'
             f'MESH = 16 16 16\n')
     f.close()
 
 
-def generate_phonon_data():
+def generate_phonon_data(bandstructure=True, thermal_properties=True):
+    """
+    Generate phonon-related data such as phonon band structures and
+    thermodynamic properties.
+
+    Parameters
+    ----------
+    bandstructure : bool, optional
+        If True (default), compute the phonon band structure along a chosen
+        high-symmetry path in the Brillouin zone using the information available in the band.conf file obtained from
+        get_band_conf function.
+
+    thermal_properties : bool, optional
+        If True (default), compute thermodynamic quantities derived from the phonon density of states (DOS), such as
+        free energy, entropy, and heat capacity as a function of temperature. This needs the thermal.conf file
+        generated using the get_thermal_conf function.
+
+    Returns
+    -------
+    None.
+
+    Runs the terminal commands and generates the following files:
+    FORCE_SETS -- force information obtained using the phonopy API. This is needed for all post-processing of data
+    band.pdf -- phonon band structure
+    band.yaml -- phonon band structure data as yaml file. use the phonon_data_to_csv to obtain the data as a csv file
+    thermal.pdf -- thermal properties plotted as a function of temperature
+    thermal_properites.yaml - thermal properties data as yaml file. use the phonon_data_to_csv to obtain the data as a
+    csv file
+
+
+    Notes
+    -----
+    To obtain the thermal properities, it is necessary to keep the paramter bandstructure=True, because thermal
+    properties are derived from phonon density of states (DOS). In that case, if thermal_properties=True and bandstructure=False,
+    the code will run the bandstructure command automatically to avoid any errors.
+    """
     import os
-    os.system("phonopy -f disp-?/aims.out")
-    os.system("phonopy -p -s band.conf")
-    os.system("phonopy -p -s thermal.conf")
+    os.system("phonopy -f disp-*/aims.out")
+    if bandstructure:
+        os.system("phonopy -p -s band.conf")
+        if thermal_properties:
+            os.system("phonopy -p -s thermal.conf")
+    else:
+        if not bandstructure:
+            if thermal_properties:
+                os.system("phonopy -p -s band.conf")
+                os.system("phonopy -p -s thermal.conf")
 
 
-def phonon_data_to_csv(band_data=False, thermal_data=True, band_file='band.yaml', thermal_file='thermal_properties.yaml'):
+def phonon_data_to_csv(band_data=False, thermal_data=True,
+                       band_file='band.yaml', thermal_file='thermal_properties.yaml'):
+    """
+    Convert phonon calculation results from YAML files into CSV format.
+
+    Parameters
+    ----------
+    band_data : bool, optional
+        If True, parse phonon band structure data from `band_file` (needs to be .yaml) and
+        export it to a CSV file. Default is False.
+    thermal_data : bool, optional
+        If True, parse phonon thermal property data from `thermal_file` (needs to be .yaml) and
+        export it to a CSV file. Default is True.
+    band_file : str, optional
+        Path to the YAML file containing phonon band structure data.
+        Default is 'band.yaml'.
+    thermal_file : str, optional
+        Path to the YAML file containing thermal property data.
+        Default is 'thermal_properties.yaml'.
+
+    Returns
+    -------
+    None
+        The function writes CSV files to disk and does not return any value.
+
+    Notes
+    -----
+    - The band structure YAML file (e.g., produced by Phonopy) typically
+      contains q-points and frequencies
+    - The thermal properties YAML file contains temperature-dependent
+      quantities such as free energy, entropy, and heat capacity.
+    - The resulting CSV files are formatted for easy plotting or further
+      numerical analysis with standard tools.
+    """
+
     import yaml
     from yaml import load
     from yaml import CLoader as Loader
