ee3d047a9df838906799074bda42266288dcc6ea
OscarvanVuren <vanvureno@cardiff.ac.uk>
2025-10-02 15:25:58 +0100

    Updated calculation of separation

--- carmm/build/append_bulk.py
+++ carmm/build/append_bulk.py
@@ -3,11 +3,14 @@ def append_bulk(input_surface, input_bulk, nlayers=None, vac=10.0, output="apend
     
     """
     Small function to append layers of bulk underneath a relaxed surface.
+    Allows for the construction of very large slab models for cutting QM/MM clusters
+    without having to relax such a large model.
     Author: Hermione Beer, Oscar van Vuren
 
     Args:
     	surface: file or atoms containing relaxed and optimised surface geometry (.traj).
     	bulk: file or atoms containing relaxed and optimised bulk geometry (.traj). Can contain the number of layers of bulk required or be constructed (nlayers).
+        NOTE: THE REPEAT UNIT OF BULK IN A SLAB MAY NOT BE THE BULK UNIT CELL. ENSURE USE OF CORRECT GEOMETRY FOR BULK
         nlayers: number of layers of bulk to append. If None, bulk will be appended as is
     	sep: distance (Angstrom) between two layers. If sep = 0, then the surface will sit directly on bulk with the distance between them the same as the distance between the first upper 2 layers of bulk
     	vac: vacuum to be added in the z direction
@@ -43,7 +46,15 @@ def append_bulk(input_surface, input_bulk, nlayers=None, vac=10.0, output="apend
     # Defining vertical distance between bulk and surf
     # finding interlayer separation in the bulk
     zeds = np.sort(np.unique(bulk.positions[:,2]))
-    separation = zeds[1] - zeds[0]
+    xyzero = [bulk.positions[0,0], bulk.positions[0,1]]
+    # Ensuring that we are only counting the separation in the z dimension
+    for ind in range(len(zeds)):
+        if bulk.positions[ind,0] == xyzero[0] and bulk.positions[ind,1] == xyzero[1]:
+            separation = separation = zeds[ind] - zeds[0]
+            break
+        else:
+            continue
+
     # Constructing shift using interlayer distance
     z_shift = bulk_max_z - surf_min_z + separation
 
