61d494a3cc29a0deb27483b5dd776f3a1a08c471
Akash Hiregange <akashhiregange1999@gmail.com>
2025-12-16 17:29:06 +0000

    Check whether other assertions work

--- examples/analyse_elastic_tensor.py
+++ examples/analyse_elastic_tensor.py
@@ -8,14 +8,12 @@ def test_compute_elasticity_tensor():
     strain_tensor = read_strain_tensor_from_pkl(f'{example_path}/strain_tensor.pkl')
     stress_tensor = read_stress_from_outputs(path=example_path,output_file_type='.xyz')
     elasticity_tensor = compute_elasticity_tensor(strain_tensor, stress_tensor=stress_tensor, path=example_path)
-    print(elasticity_tensor[0,0,0,0])
 
 
     #### Assertion test for save ####
 
     import os
     import numpy as np
-    assert(np.isclose(elasticity_tensor[0,0,0,0], -1.6533769844159, rtol=1e-8))
     assert(os.path.exists(f'{example_path}/elasticity_tensor.pkl')) # check for elasticity tensor pkl file
     assert(os.path.exists(f'{example_path}/elasticity_tensor_calculation_output.txt')) # check for output txt file
 
