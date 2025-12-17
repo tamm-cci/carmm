adc77cf0c1be3abf0bb966f1de706f6dae8ec63b
akash <akashhiregange1999@gmail.com>
2025-12-17 14:24:22 +0000

    Checked with local versions of Python 3.7, 3.8 and 3.10. everything worked fine. Minor changes to check again

--- examples/analyse_elastic_tensor.py
+++ examples/analyse_elastic_tensor.py
@@ -2,18 +2,20 @@ def test_compute_elasticity_tensor():
     #import sys
     #sys.path.append(r"C:\Users\\akash\OneDrive - Cardiff University\Desktop\FT crystals data\carmm\carmm")
     from carmm.analyse.calculate_elasticity_tensor import read_strain_tensor_from_pkl, read_stress_from_outputs, compute_elasticity_tensor
-
+    import os
     #example_path = r'C:\Users\akash\OneDrive - Cardiff University\Desktop\FT crystals data\carmm\carmm\examples\data\elasticity_tensor_workflow'
-    example_path = './data/elasticity_tensor_workflow'
+    example_path = 'data/elasticity_tensor_workflow'
     strain_tensor = read_strain_tensor_from_pkl(f'{example_path}/strain_tensor.pkl')
     stress_tensor = read_stress_from_outputs(path=example_path,output_file_type='.xyz')
+    #print(stress_tensor)
     elasticity_tensor = compute_elasticity_tensor(strain_tensor, stress_tensor=stress_tensor, path=example_path)
-
+    print(elasticity_tensor[0,0,0,0])
 
     #### Assertion test for save ####
 
     import os
     import numpy as np
+    assert(np.isclose(elasticity_tensor[0,0,0,0],2.9813036535009156,rtol=1e-8))
     assert(os.path.exists(f'{example_path}/elasticity_tensor.pkl')) # check for elasticity tensor pkl file
     assert(os.path.exists(f'{example_path}/elasticity_tensor_calculation_output.txt')) # check for output txt file
 
