992374847b179f00cb63766ee888cca63fbe46ba
Andrew Logsdail <LogsdailA@cardiff.ac.uk>
2026-06-10 13:41:57 +0200

    Rename test function and update surface generation

--- examples/build_facets_pymatgen.py
+++ examples/build_facets_pymatgen.py
@@ -4,14 +4,14 @@ This is particularly useful when making terminations for a given facet. In this 
 (111) surface of rock salt MgO which exhibits two types of terminations: O-termination and Mg-termination.
 '''
 
-def test_build_pymatgen_surface():
+def test_build_facets_pymatgen():
 
     #### Functionality to create all surfaces ####
-    from carmm.build.pymatgen_surface import generate_pymatgen_surface
+    from carmm.build.facets_pymatgen import generate
     from ase.build import bulk
     bulk_model = bulk('MgO', crystalstructure='rocksalt', cubic=True, a=4.21)
 
-    surface = generate_pymatgen_surface(bulk_model, layers=8, miller_index=(1,1,1), save=True, path='data')
+    surface = generate(bulk_model, layers=8, miller_index=(1,1,1), save=True, path='data')
 
     #### Assertion test for save ####
     import os
@@ -19,17 +19,21 @@ def test_build_pymatgen_surface():
     #########
     #
     #### Assertion tests ####
-    from ase.io import read
+    #from ase.io import read
 
     ### checking slab termination
-    atoms = read('data/sym_slab0_8_layer/geometry.in')
+    #atoms = read('data/sym_slab0_8_layer/geometry.in')
+    atoms = surface[0]
+    
     z_coordinates = atoms.get_positions()[:,2]
     import numpy as np
     # the top and bottom atom must be oxygen as it is an oxygen terminated slab
     assert atoms[np.argmax(z_coordinates)].symbol == 'O'
     assert atoms[np.argmin(z_coordinates)].symbol == 'O'
 
-    atoms = read('data/sym_slab1_8_layer/geometry.in')
+    #atoms = read('data/sym_slab1_8_layer/geometry.in')
+    atoms = surface[1]
+    
     z_coordinates = atoms.get_positions()[:, 2]
     import numpy as np
     # Here, the top and bottom atom must be Mg as it is a Magnesium terminated slab
