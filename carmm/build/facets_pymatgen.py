bffb496ecd51498eda7c5342e0a32edaa50c3bd2
Andrew Logsdail <LogsdailA@cardiff.ac.uk>
2026-06-10 12:13:30 +0200

    Update facets_pymatgen.py

--- carmm/build/facets_pymatgen.py
+++ carmm/build/facets_pymatgen.py
@@ -1,4 +1,4 @@
-def generate_pymatgen():
+def generate():
 	'''Renaming of generate_pymatgen_surface to be consistent with other files - 
 	long term the subroutine this calls should be merged into
 	this subroutine, i.e., there is not a further function call below'''
